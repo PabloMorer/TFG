e994545611251d73cf4a53d02b317483d05897f3
PabloMorer <61006664+PabloMorer@users.noreply.github.com>
2021-05-18 00:10:22 +0200

    Delete Scripts directory

--- Scripts/generateLib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo Generating Library
-gcc -I/Users/bryanrvvargas/openmpi-2.0.1/include -Wall -c traceLib.c 
-echo Copy in Programas/
-cp traceLib.o Programas/
-cp traceLib.h Programas/
